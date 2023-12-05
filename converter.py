c3871dc9e5dcf34b5555bcf6be9f378d3f03fe9f
HassanElwany <elwany_hassan@hotmail.com>
2023-12-05 10:43:00 +0300

    set up laptop

--- converter.py
+++ converter.py
@@ -1,15 +1,30 @@
-from tkinter import *
+# from tkinter import *
+# from tkinter import ttk
+# window = Tk()
+# window.minsize(width=700, height=500)
 
-window = Tk()
-window.minsize(width=700, height=500)
+# window.title("First GUI app")
 
-window.title("First GUI app")
-
-
-
-
-
+# my_label = Label(text="I am a label", font=("Arial", 24))
+# my_label.pack()
 
 
 
-window.mainloop()
+
+
+
+
+
+# window.mainloop()
+
+#-----------------------------------------------------------------------
+
+# def add(*args):
+#     result = 0
+#     for n in args:
+#         result += n
+#     print(result)
+
+# add(3, 4, 3, 13)
+
+
