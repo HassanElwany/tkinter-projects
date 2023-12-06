ff8e819732e9dfaa717e6e8aa56bf01d3cff28cf
HassanElwany <elwany_hassan@hotmail.com>
2023-12-06 10:44:50 +0300

    nothing more

--- converter.py
+++ converter.py
@@ -1,30 +1,34 @@
-# from tkinter import *
-# from tkinter import ttk
-# window = Tk()
-# window.minsize(width=700, height=500)
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+window.minsize(width=700, height=500)
 
-# window.title("First GUI app")
+window.title("First GUI app")
 
-# my_label = Label(text="I am a label", font=("Arial", 24))
-# my_label.pack()
-
-
+my_label = Label(text="I am a label", font=("Arial", 24))
+my_label.pack()
 
 
+my_label.config(text="My name is Hassan")
+
+
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+
+button = Button(text="Click here", command=button_clicked).pack()
+
+
+
+input = Entry(width=75).pack()
 
 
 
 
-# window.mainloop()
-
-#-----------------------------------------------------------------------
-
-# def add(*args):
-#     result = 0
-#     for n in args:
-#         result += n
-#     print(result)
-
-# add(3, 4, 3, 13)
 
 
+
+
+window.mainloop()
