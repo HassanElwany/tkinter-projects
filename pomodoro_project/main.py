30fc88deece330da8ac073c89b453f5250e5ccce
HassanElwany <elwany_hassan@hotmail.com>
2023-12-09 14:47:52 +0300

    add functionality to app

--- pomodoro_project/main.py
+++ pomodoro_project/main.py
@@ -1,19 +1,58 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    reps += 1
+    work_sec = WORK_MIN *60
+    short_break = SHORT_BREAK_MIN * 60
+    long_break = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        title_label.config(text="Long break", fg=RED)
+        count_down(long_break)
+    elif reps % 2 == 0:
+        title_label.config(text="Short Break", fg=PINK)
+        count_down(short_break)
+    else:
+        title_label.config(text="Work Time", fg=GREEN)
+        count_down(work_sec)
+    
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+
+    count_minute = math.floor(count / 60)
+    if count_minute < 10:
+        count_minute = f"0{count_minute}"
+
+    count_secondes =  count % 60
+    if count_secondes < 10:
+        count_secondes = f"0{count_secondes}"
+    if count_secondes == 0:
+        count_secondes = "00"
+
+    canvas.itemconfig(timer_text, text=f"{count_minute}:{count_secondes}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -26,10 +65,12 @@ title_label.grid(row=0, column=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_image = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_image)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset", highlightthickness=0)
