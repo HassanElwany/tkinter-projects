459f4abcda4e89345b16d7c69bb895c176d7e289
HassanElwany <elwany_hassan@hotmail.com>
2023-12-14 21:02:21 +0300

    trying oop method on app

--- flash-card-project/main.py
+++ flash-card-project/main.py
@@ -1,10 +1,68 @@
-with open("./german.txt", "r") as my_file:
-    data = my_file.readlines()
-    
+from tkinter import *
+import pandas
+import random
+import time
 
-words = [line.split()[0] for line in data]
+BACKGROUND_COLOR = "#B1DDC6"
 
-with open("./output.txt", "w") as output_file:
-    for word in words:
-        output_file.write(f"{word}\n")
-    
+class FlashCardApp:
+    def __init__(self, window, data):
+        self.window = window
+        self.window.title("Flash Cards")
+        self.window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+        self.flipping = window.after(3000, func=self.flip_card)
+
+        self.canvas = Canvas(width=800, height=526)
+        self.card_front_img = PhotoImage(file="./images/card_front.png")
+        self.card_back_img = PhotoImage(file="./images/card_back.png")
+        self.canvas_image = self.canvas.create_image(400, 263, image=self.card_front_img)
+        self.canvas.grid(row=0, column=0, columnspan=2)
+        self.canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+        self.card_title = self.canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+        self.card_word = self.canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+
+        self.cross_image = PhotoImage(file="./images/wrong.png")
+        self.unknown_button = Button(image=self.cross_image, highlightthickness=0, command=self.next_card)
+        self.unknown_button.grid(row=1, column=0)
+
+        self.check_image = PhotoImage(file="./images/right.png")
+        self.check_button = Button(image=self.check_image, highlightthickness=0, command=self.next_card)
+        self.check_button.grid(row=1, column=1)
+
+        self.to_learn = data.to_dict(orient="records")
+        self.current_card = {}
+        self.current_side = "German"
+        
+
+        self.next_card()
+
+    def next_card(self):
+        
+        self.window.after_cancel(self.flipping)
+        self.current_card = random.choice(self.to_learn)
+        self.canvas.itemconfig(self.card_title, text="German")
+        self.canvas.itemconfig(self.card_word, text=self.current_card["German"])
+        self.flipping = self.window.after(3000, func=self.flip_card)
+        
+        
+
+    def flip_card(self):
+        if self.current_side == "German":
+            self.canvas.itemconfig(self.card_title, text="English", fill="white")
+            self.canvas.itemconfig(self.card_word, text=self.current_card["English"], fill="white")
+            self.canvas.itemconfig(self.canvas_image, image=self.card_back_img)
+            self.current_side = "English"
+        else:
+            self.canvas.itemconfig(self.card_title, text="German", fill="black")
+            self.canvas.itemconfig(self.card_word, text=self.current_card["German"], fill="black")
+            self.canvas.itemconfig(self.canvas_image, image=self.card_front_img) 
+            self.current_side = "German"   
+               
+
+
+data = pandas.read_csv("./data/flash_card_project - Sheet1.csv")
+window = Tk()
+
+
+flash_card_app = FlashCardApp(window, data)
+window.mainloop()
